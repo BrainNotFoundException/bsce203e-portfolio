e1a8685501ac026927b8f562cd7e42fe1056cd8a
Lavnish <lavjhn@gmail.com>
2026-04-07 20:39:42 +0530

    Create basic navbar

---
+++ src/stuff/navbar.jsx
@@ -0,0 +1,12 @@
+export default function Navbar() {
+  return (
+    <nav className="nav">
+      <span><b>Lavnish Jhunjhunwala</b></span>
+      <div className="nav-links">
+        <a href="#">Work</a>
+        <a href="#">About</a>
+        <a href="#">Writing</a>
+      </div>
+    </nav>
+  );
+}
