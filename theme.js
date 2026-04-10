1309c83fca8bda5acad72a76fc6fcaeaf8e5468d
Lavnish <lavjhn@gmail.com>
2026-04-10 11:35:36 +0530

    fix theme loading issue

--- theme.js
+++
@@ -1,3 +0,0 @@
-const t = localStorage.getItem("theme") ||
-    (window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light");
-document.body.classList.toggle("dark", t === "dark");
